7388477454ec2faeb77920b2e9e16ca0726f254e
Matthias-Gilbert <89266976+Matthias-Gilbert@users.noreply.github.com>
2023-06-13 10:42:06 -0600

    Update fst_with_whitlst_cnt_check.sql

--- teradata_sql/output/bteq/BU/SIMBA/AMPS/faster_withdrawal_whitelist/fst_with_whitlst_cnt_check.sql
+++ teradata_sql/output/bteq/BU/SIMBA/AMPS/faster_withdrawal_whitelist/fst_with_whitlst_cnt_check.sql
@@ -4,6 +4,7 @@ CREATE SET TABLE my_database.employee,FALLBACK (
    LastName VARCHAR(30), 
    DOB DATE FORMAT 'YYYY-MM-DD', 
    JoinedDate DATE FORMAT 'YYYY-MM-DD', 
-   DepartmentNo BYTEINT 
+   DepartmentNo BYTEINT,
+   TIMESTAMP
 ) 
 UNIQUE PRIMARY INDEX ( EmployeeNo );
