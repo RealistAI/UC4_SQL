ec3e076490a3e081e43717f8e0acd0db369e23ab
Merrik Gilbert <merrik.gilbert@realist.ai>
2023-06-13 21:58:44 +0000

    commiting branch to the UC4 SQL repository

---
+++ bigquery_sql/translated/output/bteq/BU/SIMBA/AMPS/dw_table_current/dw_table_current_roe.sql
@@ -0,0 +1,9 @@
+-- Translation time: 2023-06-13T21:58:35.560938Z
+-- Translation job ID: 47b2d6de-6746-44db-b08d-61565526e0cc
+-- Source: dwh_preprocessed/a8c9136c-50a2-4f91-af55-3bc1718be33f/preprocessed/output/bteq/BU/SIMBA/AMPS/dw_table_current/dw_table_current_roe.sql
+-- Translated from: Teradata
+-- Translated to: BigQuery
+
+INSERT INTO `michael-gilbert-dev`.uc4_conversion_test_dataset.employee
+  VALUES (123, johnny, appleseed, DATE '2002-12-12', DATE '2020-12-12', 1182)
+;
---
+++ bigquery_sql/translated/output/bteq/BU/SIMBA/AMPS/radd_master/radd_master_upd.sql
@@ -0,0 +1,11 @@
+-- Translation time: 2023-06-13T21:58:35.560938Z
+-- Translation job ID: 47b2d6de-6746-44db-b08d-61565526e0cc
+-- Source: dwh_preprocessed/a8c9136c-50a2-4f91-af55-3bc1718be33f/preprocessed/output/bteq/BU/SIMBA/AMPS/radd_master/radd_master_upd.sql
+-- Translated from: Teradata
+-- Translated to: BigQuery
+
+SELECT
+    *
+  FROM
+    `michael-gilbert-dev`.uc4_conversion_test_dataset.employee
+;
